366bec4dcf4ee3c7b1291808545fd33b94cc0c02
MRX1T <arsenber09@gmail.com>
2023-08-13 02:06:29 +0300

    base ch

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::env::args;
-use std::process::{Command, exit};
-use libc::{kill, SIGTERM};
+use std::process::{Command, exit, id};
+use libc::{kill, getsid, SIGTERM, SIGSTOP, SIGCONT};
 
 
 // pallk - Kill !ALL! process
@@ -11,9 +11,14 @@ fn main() {
 
     let mut signal = SIGTERM;
 
+
+    sig_n1(SIGSTOP);
+
+
     for arg in &args[1..] {
         match arg.as_str() {
             "h" | "help" | "-h" | "-help" | "--help" => help(),
+            // "a" | "A" | "all" => 
             _ => {
                 if let Ok(sig) = arg.parse() { signal = sig }
                 else {
@@ -32,13 +37,21 @@ fn main() {
         .stdout;
     let output = String::from_utf8(raw_output).unwrap();
     let vector = output.split('\n'); // [0] is "PID"
+    let cur_pid = id() as i32;
     unsafe {
         for str_pid in vector {
             if let Ok(pid) = str_pid.parse() {
+                if pid < 2 || pid == cur_pid || getsid(pid) == getsid(0) || getsid(pid) == 0 { continue; }
                 kill(pid, signal);
             }
         }
     }
+
+    sig_n1(SIGCONT);
+}
+
+fn sig_n1(sig: i32) {
+    unsafe { kill(-1, sig); }
 }
 
 fn help() {
